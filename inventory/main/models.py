fa2a021e90f9206ff8b547c0529c0d76e97dd5ba
Nazmul Hossain Ananto <nazmul15-10642@diu.edu.bd>
2020-09-19 15:09:27 +0600

    main_model.py added

---
+++ inventory/main/models.py
@@ -0,0 +1,59 @@
+from django.db import models
+from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
+from django.db.models.signals import pre_save
+
+class Vendor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=120, unique=True)
+    address = models.CharField(max_length=220)
+    created_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Company_manager(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=120, unique=True)
+    address = models.CharField(max_length=220)
+    created_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=120, unique=True)
+    price = models.FloatField()
+    created_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Vendor_order(models.Model):
+    STATUS_CHOICE = (
+        ('pending', 'Pending'),
+        ('decline', 'Decline'),
+        ('approved', 'Approved'),
+    )
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICE)
+    created_date = models.DateField(auto_now_add=True)
+
+class Company_order(models.Model):
+    STATUS_CHOICE = (
+        ('delivered', 'Delivered'),
+        ('pending', 'Pending'),
+    )
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
+    company_manager = models.ForeignKey(Company_manager, on_delete=models.CASCADE, null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.CharField(max_length=120, unique=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICE)
+    created_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.product.name
