c4c3382caaa3705742f95e2237c14d0bed880bbd
soniaakhter <67204193+soniaakhter@users.noreply.github.com>
2020-09-20 11:55:07 +0600

    login

--- inventory/account/models.py
+++ inventory/account/models.py
@@ -1,18 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# class Profile(models.Model):
-#     GENDER = [
-#         ('male', 'Male'),
-#         ('female', 'Female'),
-#         ('others', 'Others'),
-#     ]
 
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     avatar = models.ImageField(
-#         upload_to='profiles', max_length=250, default='profiles/default.jpg')
-#     contact = models.CharField(max_length=50, blank=True)
-#     gender = models.CharField(choices=GENDER, max_length=250, blank=True)
+class Profile(models.Model):
+    GENDER = [
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('others', 'Others'),
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     
-    
-#     def __str__(self):
-#         return self.user.username
+    contact = models.CharField(max_length=50, blank=True)
+    gender = models.CharField(choices=GENDER, max_length=250, blank=True)
+    profession = models.CharField(max_length=50, blank=True)
+   
+
+    def __str__(self):
+        return self.user.username
--- inventory/inventory/urls.py
+++ inventory/inventory/urls.py
@@ -6,12 +6,9 @@ from account import views as account_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main_views.index, name='home'),
+    path('register/',account_views.register,name='register'),
+    path('login/',account_views.login,name='login'),
+    path('logout/',account_views.logout,name='logout'),
     
-    path('signup/', account_views.signup, name='signup'),
-    path('login/', account_views.login, name='login'),
-    # path('logout/', account_views.user_logout, name='logout'),
-    # path('logout/', account_views.logout, name='logout'),
-    # path('profile/', account_views.profile, name='profile'),
-    # path('editprofile/', account_views.editprofile, name='editprofile'),
-    # path('editpassword/', account_views.editpassword, name='editpassword'),
+    
 ]
